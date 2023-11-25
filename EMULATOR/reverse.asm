2469a749a3031503e7df85d3d33778489b430875
Mekha <mekhalnair@gmail.com>
2023-11-25 13:06:41 +0530

    adding substring

--- EMULATOR/reverse.asm
+++ EMULATOR/reverse.asm
@@ -1,90 +1,38 @@
-.MODEL SMALL
-.STACK 100H
-.DATA
+Data Segment
+  str1 db 'String_Reverse','$'  
+      strlen1 dw $-str1
+  strrev db 20 dup(' ')
+Data Ends
 
-; The string to be printed
-STRING DB 'This is a sample string', '$'
-
-.CODE
-MAIN PROC FAR
-MOV AX,@DATA
-MOV DS,AX
-
-; call reverse function
-CALL REVERSE
-
-; load address of the string
-LEA DX,STRING
-
-; output the string
-; loaded in dx
-MOV AH, 09H
-INT 21H
-
-; interrupt to exit
-MOV AH, 4CH
-INT 21H
-
-MAIN ENDP
-REVERSE PROC
-	; load the offset of
-	; the string
-	MOV SI, OFFSET STRING
-
-	; count of characters of the;
-	;string
-	MOV CX, 0H
-
-	LOOP1:
-	; compare if this is;
-	;the last character
-	MOV AX, [SI]
-	CMP AL, '$'
-	JE LABEL1
-
-	; else push it in the;
-	;stack
-	PUSH [SI]
-
-	; increment the pointer;
-	;and count
-	INC SI
-	INC CX
-
-	JMP LOOP1
-
-	LABEL1:
-	; again load the starting;
-	;address of the string
-	MOV SI, OFFSET STRING
-
-		LOOP2:
-		;if count not equal to zero
-		CMP CX,0
-		JE EXIT
-
-		; pop the top of stack
-		POP DX
-
-		; make dh, 0
-		XOR DH, DH
-
-		; put the character of the;
-		;reversed string
-		MOV [SI], DX
-
-		; increment si and;
-		;decrement count
-		INC SI
-		DEC CX
-
-		JMP LOOP2
-
-				
-	EXIT:
-	; add $ to the end of string
-	MOV [SI],'$ '
-	RET
-		
-REVERSE ENDP
-END MAIN
+Code Segment
+  Assume cs:code, ds:data
+  Begin:
+    mov ax, data
+    mov ds, ax
+    mov es, ax
+    mov cx, strlen1
+    add cx, -2
+    lea si, str1
+    lea di, strrev
+    add si, strlen1
+    add si, -2
+    L1:
+       mov al, [si]
+       mov [di], al
+       dec si
+       inc di
+       loop L1
+       mov al, [si]
+       mov [di], al
+       inc di
+       mov dl, '$'
+       mov [di], dl
+    Print:
+       mov ah, 09h
+       lea dx, strrev
+       int 21h
+Exit:
+       mov ax, 4c00h
+       int 21h
+Code Ends
+End Begin
